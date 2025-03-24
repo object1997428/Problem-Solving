2002109e776c624903e3131bed7c29949c1b9bb9
object1997 <71408968+object1997428@users.noreply.github.com>
2025-03-24 22:06:20 +0900

    [level 3] Title: 징검다리 건너기, Time: 10.86 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -9,54 +9,8 @@
 using namespace std;
 typedef pair<int,int> pii;
 typedef long long ll;
-#define endl '\n'
 
-vector<int> stones;
-int K,N;
-
-bool isValid(int k){//건너갈 수 있음
-    for(int i=0;i<N;i++) stones[i]-=k;
-    
-    // cout<<"k: "<<k<<endl;
-    // for(int i=0;i<N;i++) cout<<stones[i]<<" "; cout<<endl;
-    
-    bool ret=true;
-    for(int i=0;i<N;i++){
-        if(stones[i]<=0){
-            int cnt=0;
-            while(cnt<=K&&i+cnt<N&&stones[i+cnt]<=0) cnt++;
-            if(cnt>=K){
-                ret=false; break;
-            }
-            else i+=cnt;
-        }
-    }
-    
-    for(int i=0;i<N;i++) stones[i]+=k;
-    
-    // cout<<"ret: "<<ret<<endl;
-    return ret;
-}
-
-ll solve(){
-    ll s=0, e=200000000;
-    ll ans=0;
-    while(s<=e){
-        ll m=(s+e)/2;
-        if(isValid(m)){
-            s=m+1;
-            ans=max(ans,m);
-        }
-        else e=m-1;
-    }
-    return ans;
-}
-
-int solution(vector<int> _stones, int k) {
-    K=k; stones=_stones;
-    N=_stones.size();
-    
-    //solve
-    int answer = solve()+1;
+int solution(vector<int> stones, int k) {
+    int answer = 0;
     return answer;
 }
