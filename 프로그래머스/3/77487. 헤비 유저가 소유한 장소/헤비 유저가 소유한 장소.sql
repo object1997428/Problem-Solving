9904ad4276c3693835206be8795d98b42b809e3c
object1997 <71408968+object1997428@users.noreply.github.com>
2025-04-12 12:17:17 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,14 @@
+# select HOST_ID
+# from PLACES
+# group by HOST_ID
+# having count(ID)>=2
+
+select distinct(ID), NAME, HOST_ID
+from PLACES
+where HOST_ID in (
+    select HOST_ID
+    from PLACES
+    group by HOST_ID
+    having count(ID)>=2
+)
+order by ID
