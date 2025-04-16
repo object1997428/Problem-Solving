17d67c6025d2aa336625c59f8d38080e31d9dcc2
object1997 <71408968+object1997428@users.noreply.github.com>
2025-04-16 11:13:43 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,32 +1,11 @@
-
-
 select mSCORE as SCORE, a.EMP_NO, EMP_NAME, POSITION, EMAIL
 from HR_EMPLOYEES a
 join (
-    select aa.EMP_NO, mSCORE
-    from (
-        select EMP_NO, sum(SCORE) as sSCORE
-        from HR_GRADE
-        where YEAR = 2022
-        group by EMP_NO
-    ) as aa,
-    (
-        select max(sSCORE) as mSCORE
-        from (
-            select EMP_NO, sum(SCORE) as sSCORE
-            from HR_GRADE
-            where YEAR = 2022
-            group by EMP_NO
-        ) as b
-    ) as bb
-    where aa.sSCORE=bb.mSCORE
-) cc on cc.EMP_NO=a.EMP_NO
+    select EMP_NO, sum(SCORE) as mSCORE
+    from HR_GRADE
+    where YEAR = 2022
+    group by EMP_NO
+    order by sum(SCORE) desc
+    limit 1
+) as b on a.EMP_NO=b.EMP_NO
 
-
-
-
-
-# select EMP_NO, sum(SCORE) as sSCORE
-# from HR_GRADE
-# where YEAR
-# group by EMP_NO
