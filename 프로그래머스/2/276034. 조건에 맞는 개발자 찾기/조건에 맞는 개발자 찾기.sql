cd7d8249f593b6155930c30320f0092e54e82c09
object1997 <71408968+object1997428@users.noreply.github.com>
2025-04-20 18:03:39 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,9 +1,9 @@
-select distinct(ID), EMAIL, FIRST_NAME, LAST_NAME
-from DEVELOPERS a,
-(
+select distinct(a.ID), EMAIL, FIRST_NAME, LAST_NAME
+from DEVELOPERS a
+,(
     select CODE
     from SKILLCODES
-    where NAME='C#' or NAME='Python'
-) as b
+    where NAME='Python' or NAME='C#'
+) b
 where (a.SKILL_CODE&b.CODE)
-order by ID
+order by ID;
