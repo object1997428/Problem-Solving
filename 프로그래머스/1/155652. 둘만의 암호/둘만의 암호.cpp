cc55fc21df5059716ef7db13ba2e57d41040d0f3
object1997 <71408968+object1997428@users.noreply.github.com>
2026-01-08 09:15:03 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -2,39 +2,34 @@
 #include <vector>
 #include <string.h>
 #include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cassert>
 #include <iostream>
-#include <unordered_map>
+#include <climits>
+#include <cassert>
+#include <cmath>
 using namespace std;
 typedef pair<int,int> pii;
 typedef long long ll;
 
-unordered_map<char,int> noCount;
+int skipList[27];
 
 string solution(string s, string skip, int index) {
-    
-    for(auto k:skip){
-        noCount[k]=1;
+    for(int i=0;i<skip.size();i++){
+        char c=skip[i];
+        skipList[c-'a']=1;
     }
     
-    int N=s.size();
-    for(int i=0;i<N;i++){  
-        int j=0, count=index;
-        char nc=s[i];
-        while(j++<count){
-            nc=nc+1;
-            if(nc>'z') nc=(nc-'z')+'a'-1;
-            if(noCount[nc]==1) count++;
-            
-            cout<<"nc: "<<nc<<endl;
+    for(int i=0;i<s.size();i++){
+        char c=s[i];
+        int count=0;
+        // cout<<"s[i]: "<<s[i]<<endl;
+        while(count<index){
+            if(++c>'z') c='a';
+            if(skipList[c-'a']!=1) count++;
+            // cout<<"c: "<<c<<endl;
         }
-        s[i]=nc;
-       
+        
+        s[i]=c;
     }
-    
-    
     
     
     string answer = s;
