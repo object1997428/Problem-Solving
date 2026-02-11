f09ad0fcc8f8ca8cf620944d1b358273f169add5
object1997 <71408968+object1997428@users.noreply.github.com>
2026-02-11 09:28:40 +0900

    [level 1] Title: 실패율, Time: 1.07 ms, Memory: 9.92 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.cpp
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -10,41 +10,32 @@ using namespace std;
 typedef pair<double,int> pii;
 typedef long long ll;
 
-int sum[200002];
-double fail[200002];
-
+double sum[200002];
 vector<pii> v1;
 
-bool comp(pii& v1, pii& v2){
+bool comp(pii v1, pii v2){
     if(v1.first==v2.first) return v1.second<v2.second;
     return v1.first>v2.first;
 }
 
 vector<int> solution(int N, vector<int> stages) {
     
-    for(int i=0;i<stages.size();i++){
-        int k=stages[i];
-        sum[k]++;
+    for(auto x:stages) sum[x]++;
+    for(int i=1;i<=N+1;i++){
+        sum[i]+=sum[i-1];
     }
-    
-    for(int i=1;i<=N+1;i++) sum[i]=sum[i-1]+sum[i];
-
-    cout<<"sum[6]: "<<sum[6]<<endl;
     
     for(int i=1;i<=N;i++){
-        if(sum[N+1]-sum[i-1]==0) fail[i]=0;
-        else fail[i]=(double)(sum[i]-sum[i-1])/(sum[N+1]-sum[i-1]);
         
-        // cout<<"i: "<<i<<" stages[i]: "<<sum[i]-sum[i-1]<<" sum[N+1]-sum[i]: "<<sum[N+1]-sum[i-1]<<endl;
-        
-        v1.push_back({fail[i],i});
+        double p=0;
+        if(sum[N+1]-sum[i-1]>0) p=(sum[i]-sum[i-1])/(sum[N+1]-sum[i-1]);
+        // cout<<"i: "<<i<<" sum[i]-sum[i-1]: "<<sum[i]-sum[i-1]<<" sum[N+1]-sum[i]: "<<sum[N+1]-sum[i-1]<<endl;
+        v1.push_back({p,i});
     }
-    
     sort(v1.begin(),v1.end(),comp);
     
     vector<int> answer;
     for(int i=0;i<v1.size();i++){
-        // cout<<"v1[i].second: "<<v1[i].second<<" v1[i].first: "<<v1[i].first<<endl;
         answer.push_back(v1[i].second);
     }
     
