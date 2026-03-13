11d45be78c12bf04a4ca3c2ceec3d1f63b56c821
object1997 <71408968+object1997428@users.noreply.github.com>
2026-03-13 09:28:31 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 6.44 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -11,35 +11,32 @@ typedef pair<int,int> pii;
 typedef long long ll;
 #define endl '\n'
 
-int dp[1001][1001];
 int N,M;
+int dp[1001][1001];
 
 int solution(vector<vector<int>> board)
 {
     N=board.size(), M=board[0].size();
-    memset(dp,0,sizeof(dp));
     
     int ans=0;
-    for(int i=0;i<N;i++) dp[i][0]=board[i][0], ans=max(ans,dp[i][0]);
-    for(int i=0;i<M;i++) dp[0][i]=board[0][i], ans=max(ans,dp[0][i]);
+    for(int i=0;i<N;i++) dp[i][0]=board[i][0], ans=max(ans, board[i][0]);
+    for(int i=0;i<M;i++) dp[0][i]=board[0][i], ans=max(ans, board[0][i]);
     
-    for(int i=1;i<N;i++){
-        for(int j=1;j<M;j++){
-            if(board[i][j]==1) {
-                // if(i==1&&j==1){
-                //     cout<<dp[i-1][j-1]<<endl;
-                // }
-                dp[i][j]=min(dp[i-1][j],dp[i][j-1]);
-                dp[i][j]=min(dp[i][j],dp[i-1][j-1]);
-                dp[i][j]+=board[i][j];
-                ans=max(ans,dp[i][j]);
+    for(int i=1;i<N;i++)
+        {for(int j=1;j<M;j++){
+            if(board[i][j]==1){
+                // if(i==1&&j==1) cout<<"dp[i-1][j-1]: "<<dp[i-1][j-1]<<endl;
+                int x=min(dp[i-1][j],dp[i][j-1]);
+                x=min(x,dp[i-1][j-1]);
+
+                dp[i][j]=x+1;
             }
-            
             // cout<<dp[i][j]<<" ";
         }
-        // cout<<endl;
+         // cout<<endl;
     }
     
+    for(int i=0;i<N;i++)for(int j=0;j<M;j++) ans=max(ans,dp[i][j]);
 
     return ans*ans;
 }
