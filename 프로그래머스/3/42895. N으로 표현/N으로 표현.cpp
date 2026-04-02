cb303b0e96b8073097b01752a8008807cc863c56
object1997 <71408968+object1997428@users.noreply.github.com>
2026-04-02 10:02:23 +0900

    [level 3] Title: N으로 표현, Time: 7.01 ms, Memory: 4.66 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -10,85 +10,58 @@ using namespace std;
 typedef long long int ll;
 
 vector<ll> dp[10];
-int N;
-int ans,number;
-
+ll N;
 vector<ll> solve(int cnt){
-    if(cnt<1){
-        assert(0);
-        return {};
-    }
-    if(cnt>8){
-        assert(0);
-    }
+    // assert(cnt!=0);
+    if(cnt>8) return {};
     if(cnt==1) return {N};
     
     if(!dp[cnt].empty()) return dp[cnt];
     
-    for(int i=1;i<=cnt-1;i++){
-        if(cnt-i<1||cnt-i>8||i>8||i<1) continue;
-        auto prev=solve(cnt-i);
-        auto next=solve(i);
-        for(int j=0;j<prev.size();j++){
-            for(int k=0;k<next.size();k++){
-                ll p=prev[j];
-                ll n=next[k];
-                //더하기
-                dp[cnt].push_back(p+n);
-                //빼기
-                dp[cnt].push_back(p-n);
-                
-                //나누기
-                if(n!=0){
-                    ll dvd=(ll)floor(p/n);
-                    dp[cnt].push_back(dvd);
-                    // if(dvd==number){
-                    //      ans=min(ans,cnt);
-                    // }
+    //NNNN 만들기
+    ll NN_=0;
+    for(int i=0;i<cnt;i++) NN_=NN_*10+N;
+    dp[cnt].push_back(NN_);
+    
+    //가능한 숫자들 만들기
+    for(int i=1;i<cnt;i++){
+        vector<ll> v1=solve(i);
+        vector<ll> v2=solve(cnt-i);
+        
+        for(auto prev: v1){ //i번으로 만들수 있는 숫자
+            for(auto next: v2){ //cnt-i번으로 만들 수 있는 숫자
+                ll val1=prev+next;
+                ll val2=prev-next;
+                ll val3=prev*next;
+                if(next!=0){
+                    ll val4=(ll)floor((double)prev/next);
+                    dp[cnt].push_back(val4);
                 }
-                
-                //곱하기
-                dp[cnt].push_back(p*n);
-                // if(p+n==number||p-n==number||p*n==number){
-                //     ans=min(ans,cnt);
-                // }
+
+                dp[cnt].push_back(val1);
+                dp[cnt].push_back(val2);
+                dp[cnt].push_back(val3);
             }
         }
     }
-    //555넣기
-    ll tmp=N;
-    for(int i=0;i<cnt-1;i++){
-        tmp=tmp*10+N;
-    }
-    // cout<<"N: "<<N<<" cnt: "<<cnt<<" tmp: "<<tmp<<endl;
-    dp[cnt].push_back(tmp);
-    // if(tmp==number){
-    //     ans=cnt;
-    // }
-    //중복값 빼기
     sort(dp[cnt].begin(),dp[cnt].end());
     dp[cnt].erase(unique(dp[cnt].begin(),dp[cnt].end()),dp[cnt].end());
-    // cout<<"cnt: "<<cnt<<endl;
-    // for(int i=0;i<dp[cnt].size();i++){
-    //     cout<<"-----dp[cnt]: "<<dp[cnt][i]<<endl;
-    // }
+    
     return dp[cnt];
+    
 }
 
-int solution(int n, int _number) {
-    N=n;number=_number;
-    ans=INT_MAX;
-    if(number==n) return 1;
-    for(int i=1;i<=8;i++){
-        solve(i);
-    }
-    for(int i=1;i<=8;i++){
-        if(find(dp[i].begin(),dp[i].end(),number)!=dp[i].end()){
-            ans=i; break;
-        }
+int solution(int _N, int number) {
+    N=_N;
+    if(number==N) return 1;
+    
+    for(int i=2;i<=8;i++) solve(i);
+    
+    for(int i=2;i<=8;i++){
+        //i번째로 만든 수에 number가 있으면 바로 i리턴
+        if(dp[i].empty()) continue;
+        for(ll val:dp[i]) if(val==number) return i;
     }
     
-    int answer = ans;
-    if(ans==INT_MAX||ans>8) answer=-1;
-    return answer;
+    return -1;
 }
