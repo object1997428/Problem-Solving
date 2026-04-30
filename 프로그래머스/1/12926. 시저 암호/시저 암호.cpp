185baf63b1b1bd0c9fc83eeda8c7cd9a90f1211f
object1997 <71408968+object1997428@users.noreply.github.com>
2026-04-30 17:40:22 +0900

    [level 1] Title: 시저 암호, Time: 0.20 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <string.h>
+#include <iostream>
+#include <climits>
+#include <cassert>
+#include <cmath>
+using namespace std;
+typedef pair<int,int> pii;
+typedef long long ll;
+
+
+
+
+string solution(string s, int n) {
+    
+    string answer = "";
+    for(int i=0;i<s.size();i++){
+        char c=s[i];
+        if(c==' ') {answer+=' '; continue;}
+        
+        for(int j=0;j<n;j++){
+            c++;
+            if(s[i]>='A'&&s[i]<='Z'&&c>'Z') c='A';
+            else if(s[i]>='a'&&s[i]<='z'&&c>'z') c='a';
+        }
+        answer+=c;
+    }
+    
+    
+    
+    return answer;
+}
